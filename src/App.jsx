4be7f719497dae6bef233e017fcab71ef7581a06
ayda <aydashamma4@gmail.com>
2025-03-03 09:56:24 -0800

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/home/home";
+import { Countries } from "./components/countries/countries";
+import { About } from "./components/about/about";
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/countries" element={<Countries />}/>
+      <Route path="/about" element={<About />}/>
+
+    </Routes>
+  </div>;
 }
 
 export default App;
